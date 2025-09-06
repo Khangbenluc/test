b7faa4f7fa049cd8163e6f5c6e5d627eb41d5734
Khangbenluc <khangbenluc@gmail.com>
2025-09-06 21:14:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Ví dụ Popup với Modal")
+
+if st.button("Hiện popup"):
+    with st.modal("Thông báo"):
+        st.write("Đây là nội dung popup.")
+        if st.button("Đóng"):
+            st.rerun()
