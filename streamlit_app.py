3adfe94b174f90b75f44197d63f35bd8edeee3b7
Khangbenluc <khangbenluc@gmail.com>
2025-09-07 11:58:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ st.title("💰 Giá vàng & Biểu đồ Kitco")
 
 st.header("📊 Giá vàng hiện tại (Top Kitco)")
 st.components.v1.html("""
-<iframe src="https://www.kitco.com/charts/livegold.html" 
+<iframe src="https://www.kitco.com/charts/gold" 
     width="100%" height="500" style="border:none; overflow:hidden;"></iframe>
 """, height=500)
 
 st.header("📈 Biểu đồ giá vàng (bắt đầu từ biểu đồ Kitco)")
 st.components.v1.html("""
-<iframe src="https://www.kitco.com/charts/livegold.html" 
+<iframe src="https://www.kitco.com/charts/gold" 
     width="100%" height="500" style="border:none; overflow:hidden;"></iframe>
 """, height=500)
